c517ef5dbd1491d9cab1e84c8ad51d3aa7e18935
Deep Shivam Gautam <deepshivam@Deeps-MacBook-Pro.local>
2022-11-04 04:21:34 +0530

    Initial commit

---
+++ src/main/java/com/example/test/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GreetingController {
+
+    @Value("${my.greeting: default value}")
+    private String greetingMessage;
+
+    @Value("some static message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> val;
+
+    @GetMapping("/greeting")
+    private String greeting(){
+        return greetingMessage + " " +staticMessage +val;
+    }
+
+}
